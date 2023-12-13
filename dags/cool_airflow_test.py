4ff779e5b1f71431161f909c4053abbd28f916a4
Pascal Bürklin <pascal@buerkl.in>
2023-12-13 14:32:57 +0100

    Update pipeline

--- dags/cool_airflow_test.py
+++ dags/cool_airflow_test.py
@@ -11,15 +11,73 @@ default_args = {
 
 dag = DAG('spark_job_dag', default_args=default_args, schedule_interval=None)
 
-spark_submit_task = SparkSubmitOperator(
-    task_id='spark_submit_task',
+extract_data = SparkSubmitOperator(
+    task_id='extract_data',
     application='./spark/extract_data.py',  # Update this path
     conn_id='spark',  # Make sure this connection is configured in Airflow
     executor_memory='2g',
     total_executor_cores='1',
     name='airflow_spark_job',
-    conf={"spark.master":spark_master},
+    conf={"spark.master": spark_master},
     dag=dag,
 )
 
-spark_submit_task
+transform_core_data = SparkSubmitOperator(
+    task_id='transform_core_data',
+    application='./spark/transform_core_data.py',  # Update this path
+    conn_id='spark',  # Make sure this connection is configured in Airflow
+    executor_memory='2g',
+    total_executor_cores='1',
+    name='airflow_spark_job',
+    conf={"spark.master": spark_master},
+    dag=dag,
+)
+
+transform_error_values = SparkSubmitOperator(
+    task_id='transform_error_values',
+    application='./spark/transform_error_values.py',  # Update this path
+    conn_id='spark',  # Make sure this connection is configured in Airflow
+    executor_memory='2g',
+    total_executor_cores='1',
+    name='airflow_spark_job',
+    conf={"spark.master": spark_master},
+    dag=dag,
+)
+
+transform_free_products = SparkSubmitOperator(
+    task_id='transform_free_products',
+    application='./spark/transform_free_products.py',  # Update this path
+    conn_id='spark',  # Make sure this connection is configured in Airflow
+    executor_memory='2g',
+    total_executor_cores='1',
+    name='airflow_spark_job',
+    conf={"spark.master": spark_master},
+    dag=dag,
+)
+
+transform_revenue_data = SparkSubmitOperator(
+    task_id='transform_revenue_data',
+    application='./spark/transform_revenue_data.py',  # Update this path
+    conn_id='spark',  # Make sure this connection is configured in Airflow
+    executor_memory='2g',
+    total_executor_cores='1',
+    name='airflow_spark_job',
+    conf={"spark.master": spark_master},
+    dag=dag,
+)
+
+load_data = SparkSubmitOperator(
+    task_id='load_data',
+    application='./spark/load_data.py',  # Update this path
+    conn_id='spark',  # Make sure this connection is configured in Airflow
+    executor_memory='2g',
+    total_executor_cores='1',
+    name='airflow_spark_job',
+    conf={"spark.master": spark_master},
+    dag=dag,
+)
+
+extract_data >> transform_core_data >> load_data
+extract_data >> transform_core_data >> transform_revenue_data >> load_data
+extract_data >> transform_free_products >> load_data
+extract_data >> transform_error_values >> load_data
